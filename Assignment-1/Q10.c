e870f86c496408dcdbeddcc12181d905d9e9ad18
learningcoder05 <ns580654@gmail.com>
2024-11-11 23:01:33 +0530

    Question-8,10 added

---
+++ Assignment-1/Q10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, c = 1,i,j,space;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 0; i < rows; i++)
+    {
+        for (space = 1; space <= rows - i; space++)
+            printf("  ");
+
+        for (j = 0; j <= i; j++)
+        {
+            if (j == 0 || i == 0)
+                c = 1;
+            else
+                c = c * (i - j + 1) / j;
+
+            printf("%4d", c);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
